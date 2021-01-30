906607f4b3d79ff8e93de2c2a96ea2bd00f3a318
かずき <skaz0110@yahoo.co.jp>
2021-01-30 18:47:35 +0900

    コメント機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,18 +9,30 @@ class CommentsController < ApplicationController
   def show
   end
 
-  def create
-    @comment = Comment.new(comment_params)
-    @comment.user_id = current_user.id
-  
+  # def create
+  #   @comment = Comment.new(comment_params)
+  #   @comment.user_id = current_user.id
     
-    
-    if @comment.save
-      redirect_back(fallback_location: root_path)
-    else
-      redirect_back(fallback_location: root_path)
-    end
+  #   if @comment.save
+  #     redirect_back(fallback_location: root_path)
+  #   else
+  #     redirect_back(fallback_location: root_path)
+  #   end
 
+  # end
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    if @comment.save
+      redirect_back(fallback_location: root_path)  #コメント送信後は、一つ前のページへリダイレクトさせる。
+    else
+      redirect_back(fallback_location: root_path)  #同上
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :post_id)  #formにてpost_idパラメータを送信して、コメントへpost_idを格納するようにする必要がある。
   end
 
   def edit
@@ -32,8 +44,8 @@ class CommentsController < ApplicationController
   def destroy
   end
 
-  private
-  def comment_params
-    params.require(:comment).permit(:content)
-  end
+  # private
+  # def comment_params
+  #   params.require(:comment).permit(:content)
+  # end
 end
