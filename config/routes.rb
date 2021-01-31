dc507433b372482b31f0ace61d2eaccde2ca67bb
かずき <skaz0110@yahoo.co.jp>
2021-01-31 16:51:23 +0900

    コメント編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   post '/posts/guest_sign_in', to: 'posts#new_guest'
   resources :posts, only: [:index, :show, :create] do
-    resources :comments, only: [:create,:destroy,:edit]
+    resources :comments, only: [:create,:destroy,:edit,:update]
   end
   root 'posts#index'
 
