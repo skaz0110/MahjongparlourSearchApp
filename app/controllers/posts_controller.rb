52223d876e71a6fbc4686bf39aca736e7c34b6b3
かずき <skaz0110@yahoo.co.jp>
2021-01-31 13:34:16 +0900

    UIの調整　ヘッダーを部分テンプレート化　ヘッダーを固定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,15 @@
 class PostsController < ApplicationController
     def index
         if user_signed_in?
-            # @posts = current_user.posts.all  #投稿一覧を表示させるために全取得
-            # @post = current_user.posts.new   #投稿一覧画面で新規投稿を行うので、formのパラメータ用にPostオブジェクトを取得    
-            @posts = Post.all
-            @post = Post.new
+            
+            # 投稿一覧を表示させるために全取得
+            # @posts = current_user.posts.all
+            
+            # 投稿一覧画面で新規投稿を行うので、formのパラメータ用にPostオブジェクトを取得  
+            # @post = current_user.posts.new       
+            
+          @posts = Post.all
+          @post = Post.new
         else
           redirect_to new_user_session_path
         end
@@ -14,25 +19,36 @@ class PostsController < ApplicationController
       def show
         
         @post = Post.find(params[:id])
-        @comments = @post.comments  #投稿詳細に関連付けてあるコメントを全取得
-        @comment = current_user.comments.new  #投稿詳細画面でコメントの投稿を行うので、formのパラメータ用にCommentオブジェクトを取得
+        
+        # 投稿詳細に関連付けてあるコメントを全取得
+        @comments = @post.comments  
+        
+        # 投稿詳細画面でコメントの投稿を行うので、formのパラメータ用にCommentオブジェクトを取得
+        @comment = current_user.comments.new  
 
         @shopdetails = ShopDetail.order(:id)
                
         if user_signed_in?
           
         else
-          redirect_to user_session_path
+
+            # ログインしてください
+            redirect_to user_session_path
         end
       end
 
       def create
         
         @post = current_user.posts.new(post_params)
+        
         if @post.save
-          redirect_back(fallback_location: root_path)  #コメント送信後は、一つ前のページへリダイレクトさせる。
+          
+            #　コメント送信後は、一つ前のページへリダイレクトさせる。
+            redirect_back(fallback_location: root_path)  
         else
-          redirect_back(fallback_location: root_path)  #同上
+            
+            #　コメント送信後は、一つ前のページへリダイレクトさせる。
+            redirect_back(fallback_location: root_path) 
         end
       end
     
