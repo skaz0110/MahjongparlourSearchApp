219d0180f300c100d90a3e56993f740c715cf4ea
かずき <skaz0110@yahoo.co.jp>
2021-01-31 15:08:13 +0900

    コメント削除機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    Comment.find_by(id: params[:id],post_id: params[:post_id]).destroy
+    redirect_back(fallback_location: root_path)
+  end
+  
+  
+
   private
   def comment_params
     params.require(:comment).permit(:content, :post_id)  #formにてpost_idパラメータを送信して、コメントへpost_idを格納するようにする必要がある。
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   post '/posts/guest_sign_in', to: 'posts#new_guest'
   resources :posts, only: [:index, :show, :create] do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create,:destroy,:edit]
   end
   root 'posts#index'
 
