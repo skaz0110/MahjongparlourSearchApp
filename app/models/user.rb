969dbd314686e0e8636a26cff89738b4f8141cf9
かずき <skaz0110@yahoo.co.jp>
2021-02-06 13:48:40 +0900

    リンクでのいいね機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
-def self.guest
-  find_or_create_by!(email: 'guest@example.com') do |user|
-    user.password = SecureRandom.urlsafe_base64
-    # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+  
+  def liked_by?(post_id)
+    likes.where(post_id: post_id).exists?
+  end
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
   end
 end
