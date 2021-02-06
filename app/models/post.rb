6463585e1b1ff5796bac40ada67f5722204b1c4c
かずき <skaz0110@yahoo.co.jp>
2021-02-06 11:59:48 +0900

    アソシエーションの定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
-    has_many :comments, dependent: :destroy 
+    has_many :comments, dependent: :destroy
+    has_many :likes 
     has_one :shop_detail
 end
