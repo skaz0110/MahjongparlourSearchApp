d829d144bab8b6dcdc894d0285ef72d35d437b7d
かずき <skaz0110@yahoo.co.jp>
2021-02-11 00:34:57 +0900

    ルーティングの修正とヘッダーの追記

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,18 @@
 class LikesController < ApplicationController
-  before_action :post_params
+  #before_action :post_params
 
   def index
     
   end
   
   def create
+    @post = Post.find(params[:id])
     Like.create(user_id: current_user.id, post_id: params[:id])
     
   end
 
   def destroy
+    @post = Post.find(params[:id])
     Like.find_by(user_id: current_user.id, post_id: params[:id]).destroy
     
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
   post    'like/:id' => 'likes#create',   as: 'create_like'
   delete  'like/:id' => 'likes#destroy',  as: 'destroy_like'
-  get     'like/:id' => 'likes#index',    as: 'index_like'
+  resources :likes, only: [:index]
   root                  'homes#index'
   
 end
