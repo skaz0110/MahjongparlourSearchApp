7352974196314edafa227ba029b3959c1d9a1f48
かずき <skaz0110@yahoo.co.jp>
2021-02-11 11:14:33 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  post  '/posts/guest_sign_in', to: 'posts#new_guest'
+  root  'homes#index'
+  post  '/posts/guest_sign_in',         to: 'posts#new_guest'
+  post  'like/:id' => 'likes#create',   as: 'create_like'
+  resources :likes, only: [:index,:destroy]
   resources :homes, only: [:index]
   resources :posts, only: [:index, :show, :create] do
     resources :comments, only: [:index,:create,:destroy,:edit,:update]
   end
-  post    'like/:id' => 'likes#create',   as: 'create_like' 
-  resources :likes, only: [:index,:destroy]
-  root                  'homes#index'
-  
+   
 end
