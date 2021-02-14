26324f6e0a340d207a5d6c97102b211028de8dac
かずき <skaz0110@yahoo.co.jp>
2021-02-14 23:19:01 +0900

    管理者用モデルの生成

---
+++ app/admin/posts.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register Post do
+
+  permit_params :content,:image,:title,:user_id
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :image, :content, :user_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :image, :content, :user_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
