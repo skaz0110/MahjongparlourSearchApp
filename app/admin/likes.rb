0b6d1b268b0178a4ccff582d9ce537c40ac5dbd7
かずき <skaz0110@yahoo.co.jp>
2021-02-16 21:31:50 +0900

    管理者画面へLikeテーブルを追加

--- app/admin/likes.rb
+++ app/admin/likes.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Like do
 
-  
+  permit_params :user_id, :post_id
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
