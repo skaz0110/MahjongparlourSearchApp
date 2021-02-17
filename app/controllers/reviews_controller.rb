9d5ca22a984ff9a03f74be289385ff4cd5bcf9f2
かずき <skaz0110@yahoo.co.jp>
2021-02-17 23:31:29 +0900

    モデルの名前変更のためコントローラーの修正

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,40 @@
+class ReviewsController < ApplicationController
+
+  def index
+     
+    @post = Post.find(params[:post_id])
+    @review = current_user.reviews.new  
+    
+  end
+  
+  def create
+    @review = current_user.reviews.new(review_params)
+    
+    if @review.save
+
+      # 投稿の詳細ページへ戻る
+      redirect_to post_path(params[:post_id])
+    else
+
+      # 一つ前のページへ戻る
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def destroy
+    Review.find_by(id: params[:id],post_id: params[:post_id]).destroy
+    redirect_back(fallback_location: root_path)
+  end
+  
+  def edit
+    @review =Review.find_by(id: params[:id],post_id: params[:post_id])
+    
+  end
+  
+
+  private
+  def review_params
+    params.require(:review).permit(:content,:star, :post_id)  #formにてpost_idパラメータを送信して、コメントへpost_idを格納するようにする必要がある。
+  end
+
+end
