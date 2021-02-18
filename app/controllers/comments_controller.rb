bfb8a053fcba341a2b7388a24bfadcdfde725186
かずき <skaz0110@yahoo.co.jp>
2021-02-18 23:55:31 +0900

    コメント機能をビューで実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,10 @@
 class CommentsController < ApplicationController
 
   def index
-    #@post = Post.find(params[:post_id])
-    @comments = Comment.all
+    @post = Post.find(params[:post_id])
+    
+    # 投稿詳細に関連付けてあるコメントを全取得
+    @comments = @post.comments  
   end
 
   def create
