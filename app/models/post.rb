e2d7382b5cc39752dc379a6f0c2edff4798641dd
かずき <skaz0110@yahoo.co.jp>
2021-02-18 21:21:25 +0900

    アソシエーションの変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,8 @@ class Post < ApplicationRecord
     has_many :reviews, dependent: :destroy
     has_many :likes 
     has_one :shop_detail
+    has_many :comments
+
+    # 画像用
     mount_uploader :image, ImageUploader
 end
