b73aa5d888c55529ff6acd209c9d053548cdbe99
かずき <skaz0110@yahoo.co.jp>
2021-02-20 00:12:14 +0900

    コメント機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,18 +4,30 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     
     # 投稿詳細に関連付けてあるコメントを全取得
-    @comments = @post.comments  
+    @comments = @post.comments
+    @comment = current_user.comments.new  
   end
 
   def create
+
+    @comment = current_user.comments.new(comment_params)  
     
+    if @comment.save
+
+      # 投稿の詳細ページへ戻る
+      redirect_to post_comments_path(params[:post_id])
+    else
+
+      # 一つ前のページへ戻る
+      redirect_back(fallback_location: root_path)
+    end
   end
   
   
   
   private
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content,:post_id)
   end
   
 end
