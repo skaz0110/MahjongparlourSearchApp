ab03458adc8a11662b441a387b2d4b22bfb09dee
かずき <skaz0110@yahoo.co.jp>
2021-02-27 16:35:42 +0900

    migrateファイルの不具合を修正お

--- db/migrate/20210116092522_create_reviews.rb
+++ db/migrate/20210116092522_create_reviews.rb
@@ -3,7 +3,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
     create_table :reviews do |t|
       t.string :content
       t.references :user, null: false, foreign_key: true
-
+      t.references :post, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -88,9 +88,9 @@ ActiveRecord::Schema.define(version: 2021_02_22_124814) do
   create_table "reviews", force: :cascade do |t|
     t.string "content"
     t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "post_id"
     t.float "star"
     t.index ["post_id"], name: "index_reviews_on_post_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
