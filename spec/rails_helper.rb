5398dddae7b315c4aa4eef1dd619872e5cd523b7
kazu <skaz0110@yahoo.co.jp>
2021-04-10 14:08:59 +0900

    日本語のダミーデータの設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,10 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+# 日本語のダミーデータを使用
+Faker::Config.locale = :ja
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -31,7 +35,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  
+
   # 使用する際に FactoryBot を省略できるようにする
   config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
