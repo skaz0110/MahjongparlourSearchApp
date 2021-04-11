98fa1fc4bd9cc2c018f2d8186af87b691fa41093
kazu <skaz0110@yahoo.co.jp>
2021-04-11 14:59:07 +0900

    テストの準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,17 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments, dependent: :destroy
   
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  # バリデーション
+  validates :username, presence: true, length: { maximum: 30 }
+  # validates :image, presence: true, numericality: true, inclusion: { in: 0..150 }
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }
+
   ##  画像用
   mount_uploader :image, ImageUploader
 
