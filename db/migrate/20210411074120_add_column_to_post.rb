4b9c6a6984872deb896ae11bfa5e91143c729a64
kazu <skaz0110@yahoo.co.jp>
2021-04-11 16:48:59 +0900

    店舗情報のテーブルカラムを変更

---
+++ db/migrate/20210411074120_add_column_to_post.rb
@@ -0,0 +1,17 @@
+class AddColumnToPost < ActiveRecord::Migration[6.1]
+  def change
+    
+    # 削除
+    remove_column :posts, :address, :string
+    remove_column :posts, :phonenumber, :integer
+    
+    # 追加
+    add_column :posts, :phonenumber, :string
+    add_column :posts, :prefecturecode, :integer
+    add_column :posts, :postalcode, :integer
+    add_column :posts, :address1, :string
+    add_column :posts, :address2, :string
+    add_column :posts, :address3, :string
+
+  end
+end
