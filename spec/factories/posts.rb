26958755b506794cf16cefc0b9aae0214274d8e9
kazu <skaz0110@yahoo.co.jp>
2021-04-17 15:01:33 +0900

    Postモデルのテストコードを作成

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,6 +2,11 @@ FactoryBot.define do
   factory :post do
     title { Faker::Lorem.word }
     content { Faker::Lorem.paragraph }
+    postalcode { Faker::Address.zip }
+    prefecturecode{ Faker::Number.within(range: 1..47) }
+    address1{ Faker::Address.city }
+    address2{ Faker::Address.street_name }
+    phonenumber{ Faker::PhoneNumber.phone_number }
     user
   end
 end
