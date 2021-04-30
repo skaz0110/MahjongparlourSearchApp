dabb07a09ec3166087fe54d6dc688bdeebcb48d3
kazu <skaz0110@yahoo.co.jp>
2021-04-30 18:30:07 +0900

    Likeコントローラーのテストコードを作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,17 +6,13 @@ class LikesController < ApplicationController
   
   def create
     @post = Post.find(params[:id])
-    Like.create(user_id: current_user.id, post_id: params[:id])
+    
+    Like.create(user_id: current_user.id, post_id: @post.id)
   end
 
   def destroy
     @post = Post.find(params[:id])
-    Like.find_by(user_id: current_user.id, post_id: params[:id]).destroy
+    Like.find_by(user_id: current_user.id, post_id: @post.id).destroy
   end
 
-  private
-    def post_params
-      @post = Post.find(params[:id])
-    end
-    
 end
--- spec/requests/likes_request_spec.rb
+++ spec/requests/likes_request_spec.rb
@@ -30,52 +30,56 @@ RSpec.describe "Likes", type: :request do
     end
   end
 
-  describe "POST #create" do
-    
-    subject { post(post_comments_path(posttest.id), params: params) }
-    let(:posttest) { create(:post) }
-    let(:user) { create(:user) } 
-    before do
-      sign_in user
-    end
-
-    context "パラメータが正常なとき" do
-
-      let(:params) { { comment: attributes_for(:comment) } }
+  #TODO 非同期のテストを実施
+  # describe "POST #create" do
       
-      it "リクエストが成功する" do
-        subject 
-        expect(response).to have_http_status(302)      
-      end
+  #   let(:parapost) { create(:post) }
+  #   let(:user) { create(:user) } 
+  #   let(:like) { create(:like, user_id: user.id, post_id: parapost.id) }
 
-      it "コメントが保存される" do
-        expect { subject }.to change { Comment.count }.by(0)
-      end
+  #   before do
+  #     sign_in user
+  #   end
 
-      it "詳細ページにリダイレクトされる" do
-        subject
-        expect(response).to redirect_to Comment.last
-      end
-    end
+  #   context "パラメータが正常なとき" do
 
-    context "パラメータが異常なとき" do
+  #     it "リクエストが成功する" do
+  #       post :create, format: :json,
+  #       params: { post_id: parapost.id, user_id: user.id }
+  #       expect(response.content_type).to eq "application/json"      
+  #     end
 
-      let(:params) { { comment: attributes_for(:comment,:commentinvalid) } }
+  #     it "コメントが保存される" do
+  #       expect { subject }.to change { Like.count }.by(1)
+  #     end
 
-      it "リクエストが成功する" do
-        subject 
-        expect(response).to have_http_status(302)      
-      end
+  #     it "詳細ページにリダイレクトされる" do
+  #       subject
+  #       expect(response).to redirect_to Like.last
+  #     end
+  #   end
 
-      it "コメントが保存されない" do
-        expect { subject }.not_to change(Comment, :count)
-      end
+  #   context "パラメータが異常なとき" do
 
-      it "詳細ページがレンダリングされる" do
-        subject
-        expect(response.body).to include "You are being"
-      end
-    end
-  end
+  #     let(:params) { { comment: attributes_for(:comment,:commentinvalid) } }
+
+  #     it "リクエストが成功する" do
+  #       subject 
+  #       expect(response).to have_http_status(302)      
+  #     end
+
+  #     it "コメントが保存されない" do
+  #       expect { subject }.not_to change(Comment, :count)
+  #     end
+
+  #     it "詳細ページがレンダリングされる" do
+  #       subject
+  #       expect(response.body).to include "You are being"
+  #     end
+  #   end
+  # end
+
+  # describe "POST #create" do
+  # end
   
 end
