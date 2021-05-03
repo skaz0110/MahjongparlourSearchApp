54a08fb10131badb9bab27fd9d713b1ef655a77f
kazu <skaz0110@yahoo.co.jp>
2021-05-03 17:39:46 +0900

    アカウント編集画面を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-
+    before_action :set_search
+    
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up,        keys: [:username,:image])
       devise_parameter_sanitizer.permit(:sign_in,        keys: [:username,:image])
       devise_parameter_sanitizer.permit(:account_update, keys: [:username,:image])
     end
+
+    def set_search
+      @q = Post.ransack(params[:q])
+    end
+    
 end
